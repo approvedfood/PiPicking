059e8704289f2a2e0d026b94587e9f81a436b7cb
danielcluderay <dan@approvedfood.co.uk>
2016-12-12 10:37:03 +0000

    Now keeping track of what the next new tray is
    Introduced:
    New Job - Resets everything
    New Tray - Get the next empty tray location
    
    Still supporting moving to new trays

--- PiPick.py
+++ PiPick.py
@@ -4,7 +4,9 @@ import datetime
 
 
 class GuiSetup:
-
+    nextlocationpick = ""
+    lastpicklocation = ""
+    traycount=0
     def __init__(self, master):
         topframe = Frame(master)
         topframe.grid(row=0)
@@ -21,7 +23,7 @@ class GuiSetup:
         self.entry_userid = Entry(topframe, textvariable=v)
         self.entry_userid.grid(row=0, column=1)
 
-        def subscribe_client(self):
+        def subscribe_client():
             channel = v.get()
             print(channel)
             # self.f = str(self.lbl_connectionstatus['text'])
@@ -49,7 +51,7 @@ class GuiSetup:
         # Row 0
         prod0frame = Frame(midframe)
         prod0frame.grid(row=0, column=0)
-        self.lblprod0 = Label(prod0frame, text="Location 1")
+        self.lblprod0 = Label(prod0frame, text="Location A")
         self.lblprod0.grid(row=0, column=0)
         self.lblprod0status = Label(prod0frame, text="Status", bg="grey")
         self.lblprod0status.grid(row=0, column=1)
@@ -57,7 +59,7 @@ class GuiSetup:
         # Row 1
         prod1frame = Frame(midframe)
         prod1frame.grid(row=1, column=0)
-        self.lblprod1 = Label(prod1frame, text="Location 2")
+        self.lblprod1 = Label(prod1frame, text="Location B")
         self.lblprod1.grid(row=1, column=0)
         self.lblprod1status = Label(prod1frame, text="Status", bg="grey")
         self.lblprod1status.grid(row=1, column=1)
@@ -65,7 +67,7 @@ class GuiSetup:
         # Row 2
         prod2frame = Frame(midframe)
         prod2frame.grid(row=2, column=0)
-        self.lblprod2 = Label(prod2frame, text="Location 3")
+        self.lblprod2 = Label(prod2frame, text="Location C")
         self.lblprod2.grid(row=2, column=0)
         self.lblprod2status = Label(prod2frame, text="Status", bg="grey")
         self.lblprod2status.grid(row=2, column=1)
@@ -73,7 +75,7 @@ class GuiSetup:
         # Row 3
         prod3frame = Frame(midframe)
         prod3frame.grid(row=3, column=0)
-        self.lblprod3 = Label(prod3frame, text="Location 4")
+        self.lblprod3 = Label(prod3frame, text="Location D")
         self.lblprod3.grid(row=3, column=0)
         self.lblprod3status = Label(prod3frame, text="Status", bg="grey")
         self.lblprod3status.grid(row=3, column=1)
@@ -81,7 +83,7 @@ class GuiSetup:
         # Row 4
         prod4frame = Frame(midframe)
         prod4frame.grid(row=4, column=0)
-        self.lblprod4 = Label(prod4frame, text="Location 5")
+        self.lblprod4 = Label(prod4frame, text="Location E")
         self.lblprod4.grid(row=4, column=0)
         self.lblprod4status = Label(prod4frame, text="Status", bg="grey")
         self.lblprod4status.grid(row=4, column=1)
@@ -89,7 +91,7 @@ class GuiSetup:
         # Row 5
         prod5frame = Frame(midframe)
         prod5frame.grid(row=5, column=0)
-        self.lblprod5 = Label(prod5frame, text="Location 6")
+        self.lblprod5 = Label(prod5frame, text="Location F")
         self.lblprod5.grid(row=5, column=0)
         self.lblprod5status = Label(prod5frame, text="Status", bg="grey")
         self.lblprod5status.grid(row=5, column=1)
@@ -97,7 +99,7 @@ class GuiSetup:
         # Row 6
         prod6frame = Frame(midframe)
         prod6frame.grid(row=6, column=0)
-        self.lblprod6 = Label(prod6frame, text="Location 7")
+        self.lblprod6 = Label(prod6frame, text="Location G")
         self.lblprod6.grid(row=6, column=0)
         self.lblprod6status = Label(prod6frame, text="Status", bg="grey")
         self.lblprod6status.grid(row=6, column=1)
@@ -105,7 +107,7 @@ class GuiSetup:
         # Row 7
         prod7frame = Frame(midframe)
         prod7frame.grid(row=7, column=0)
-        self.lblprod7 = Label(prod7frame, text="Location 8")
+        self.lblprod7 = Label(prod7frame, text="Location H")
         self.lblprod7.grid(row=7, column=0)
         self.lblprod7status = Label(prod7frame, text="Status", bg="grey")
         self.lblprod7status.grid(row=7, column=1)
@@ -137,6 +139,46 @@ class GuiSetup:
             t.set("approvedfood/picking/")
             # self.lbl_connectionstatus['text'] = "approvedfood/picking/"
 
+        def getnextemptytray():
+
+
+            if self.traycount == 0:
+                self.nextlocationpick = "A"
+                self.lastpicklocation = "A"
+                self.traycount = self.traycount + 1
+            elif self.traycount == 1:
+                self.nextlocationpick = "B"
+                self.lastpicklocation = "A"
+                self.traycount = self.traycount + 1
+            elif self.traycount == 2:
+                self.nextlocationpick = "C"
+                self.lastpicklocation = "B"
+                self.traycount = self.traycount + 1
+            elif self.traycount == 3:
+                self.nextlocationpick = "D"
+                self.lastpicklocation = "C"
+                self.traycount = self.traycount + 1
+            elif self.traycount == 4:
+                self.nextlocationpick = "E"
+                self.lastpicklocation = "D"
+                self.traycount = self.traycount + 1
+            elif self.traycount == 5:
+                self.nextlocationpick = "F"
+                self.lastpicklocation = "E"
+                self.traycount = self.traycount + 1
+            elif self.traycount == 6:
+                self.nextlocationpick = "G"
+                self.lastpicklocation = "F"
+                self.traycount = self.traycount + 1
+            elif self.traycount == 7:
+                self.nextlocationpick = "H"
+                self.lastpicklocation = "G"
+                self.traycount = self.traycount + 1
+            elif self.traycount == 8:
+                # Turn lights off
+                self.nextlocationpick = "LightsOff"
+                self.lastpicklocation = "H"
+                self.traycount = self.traycount + 1
     # The callback for when a PUBLISH message is received from the server.
         def on_message(client, userdata, msg):
             print(str(msg.payload))
@@ -149,22 +191,61 @@ class GuiSetup:
             self.lblprod5status['bg'] = "grey"
             self.lblprod6status['bg'] = "grey"
             self.lblprod7status['bg'] = "grey"
-            if str(msg.payload) == "b'A'":
+
+            if str(msg.payload) == "b'New Job'":
+                # Rest the next pick job
+                self.nextlocationpick = ""
+                self.lastpicklocation = ""
+                self.traycount = 0
+
+            isnewtray = 0
+            if str(msg.payload) == "b'New Tray'":
+                isnewtray = 1
+                # Rest the next pick job
+                getnextemptytray()
+
+
+            if str(msg.payload) == "b'A'" or (isnewtray == 1 and  self.nextlocationpick == "A"):
+                self.nextlocationpick = "B"
+                self.lastpicklocation = "A"
                 self.lblprod0status['bg'] = "green"
-            elif str(msg.payload) == "b'B'":
+            elif str(msg.payload) == "b'B'" or (isnewtray == 1 and  self.nextlocationpick == "B"):
+                self.nextlocationpick = "C"
+                self.lastpicklocation = "B"
                 self.lblprod1status['bg'] = "green"
-            elif str(msg.payload) == "b'C'":
+            elif str(msg.payload) == "b'C'" or (isnewtray == 1 and self.nextlocationpick == "C"):
+                self.nextlocationpick = "D"
+                self.lastpicklocation = "C"
                 self.lblprod2status['bg'] = "green"
-            elif str(msg.payload) == "b'D'":
+            elif str(msg.payload) == "b'D'" or (isnewtray == 1 and self.nextlocationpick == "D"):
+                self.nextlocationpick = "E"
+                self.lastpicklocation = "D"
                 self.lblprod3status['bg'] = "green"
-            elif str(msg.payload) == "b'E'":
+            elif str(msg.payload) == "b'E'" or (isnewtray == 1 and self.nextlocationpick == "E"):
+                self.nextlocationpick = "F"
+                self.lastpicklocation = "E"
                 self.lblprod4status['bg'] = "green"
-            elif str(msg.payload) == "b'F'":
+            elif str(msg.payload) == "b'F'" or (isnewtray == 1 and self.nextlocationpick == "F"):
+                self.nextlocationpick = "G"
+                self.lastpicklocation = "F"
                 self.lblprod5status['bg'] = "green"
-            elif str(msg.payload) == "b'G'":
+            elif str(msg.payload) == "b'G'" or (isnewtray == 1 and self.nextlocationpick == "G"):
+                self.nextlocationpick = "H"
+                self.lastpicklocation = "G"
                 self.lblprod6status['bg'] = "green"
-            elif str(msg.payload) == "b'H'":
+            elif str(msg.payload) == "b'H'" or (isnewtray == 1 and self.nextlocationpick == "H"):
+                self.nextlocationpick = ""
+                self.lastpicklocation = "H"
                 self.lblprod7status['bg'] = "green"
+            elif str(self.nextlocationpick) == "LightsOff":
+                self.lblprod0status['bg'] = "grey"
+                self.lblprod1status['bg'] = "grey"
+                self.lblprod2status['bg'] = "grey"
+                self.lblprod3status['bg'] = "grey"
+                self.lblprod4status['bg'] = "grey"
+                self.lblprod5status['bg'] = "grey"
+                self.lblprod6status['bg'] = "grey"
+                self.lblprod7status['bg'] = "grey"
 
         client = mqtt.Client()
         client.on_connect = on_connect
